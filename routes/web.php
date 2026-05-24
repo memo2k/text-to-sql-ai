815763eba9ac4f1d39671a499f2e537643cbf23a
Mehmed <mehmedm325@gmail.com>
2026-05-24 20:34:47 +0300

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\TextToSqlController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TextToSqlController::class, 'index'])->name('text-to-sql');
+Route::post('/', [TextToSqlController::class, 'generate'])->name('text-to-sql.generate');
